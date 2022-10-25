c17ce00bcde816d108e54f6ca0c5f9ab643d8f68
Sahil Sachdev <sahil.sachdev@inai.io>
2022-10-26 00:34:54 +0530

    data and icon and logos

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const fileData = [
+  {
+    "id": "1",
+    "name": "Notes  .png",
+    "size": "20MB"
+  },
+  {
+    "id": "2",
+    "name": "Books.png",
+    "size": "5MB"
+  },
+  {
+    "id": "3",
+    "name": "Pan.png",
+    "size": "10MB"
+  },
+  {
+    "id": "4",
+    "name": "Aadhar.png",
+    "size": "80MB"
+  },
+  {
+    "id": "5",
+    "name": "Movie.png",
+    "size": "16MB"
+  },
+  {
+    "id": "6",
+    "name": "Game.png",
+    "size": "11MB"
+  },
+  {
+    "id": "7",
+    "name": "Gta.png",
+    "size": "21MB"
+  },
+  {
+    "id": "8",
+    "name": "Kids.png",
+    "size": "2MB"
+  },
+  {
+    "id": "9",
+    "name": "Dogs.png",
+    "size": "20MB"
+  },
+  {
+    "id": "10",
+    "name": "Chocolates.png",
+    "size": "20MB"
+  }
+]
